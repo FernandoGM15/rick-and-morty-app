9e57bab2724740f8c90b6497f8e633a9427e9322
Fernando <lgarcia@tredicom.com>
2023-02-22 12:36:41 -0600

    refactor: :recycle: changing the path of characterLsitHook

--- src/src/features/Characters/components/CharactersList.tsx
+++ src/src/features/Characters/components/CharactersList.tsx
@@ -1,13 +1,14 @@
-import { Box, Pagination, Typography } from "@mui/material";
-import { ChangeEvent } from "react";
-import { CharacterCard } from "./CharacterCard";
-import { useCharacterList } from "../../../hooks/useCharacterList";
+import { Box, Pagination, Typography } from '@mui/material';
+import { ChangeEvent, useState } from 'react';
+import { CharacterCard } from './CharacterCard';
+import { useCharacterList } from '../hooks/useCharacterList';
+import { LoadingSpinner } from '../../../components/LoadingSpinner/components/LoadingSpinner';
 
 export const CharactersList = () => {
   /**
    * @description Get the info provided by the custom hook
    */
-  const { characters, info, page, changePagination } = useCharacterList();
+  const { characters, info, page, changePagination, loading } = useCharacterList();
 
   /**
    * @description Executes the function of the custom hook when in the Pagination change event
@@ -19,28 +20,42 @@ export const CharactersList = () => {
   };
 
   return (
-    <Box>
-      <Typography color="primary" variant="h3" sx={{ marginBottom: "2.5rem" }}>
-        Characters
-      </Typography>
-      <Box
-        display={"grid"}
-        gridTemplateColumns={"repeat(auto-fit, minmax(250px, 1fr))"}
-        gap={"2rem"}
-        margin={"0 1rem"}
-      >
-        {characters.map((character) => {
-          return (
-            <CharacterCard
-              key={character.id}
-              id={character.id}
-              name={character.name}
-              image={character.image}
-            />
-          );
-        })}
-      </Box>
-      <Pagination count={info?.pages} page={page} onChange={handleChange} />
-    </Box>
+    <>
+      {loading ? (
+        <LoadingSpinner />
+      ) : (
+        <Box>
+          <Typography
+            color="primary"
+            variant="h3"
+            sx={{ marginBottom: '2.5rem' }}
+          >
+            Characters
+          </Typography>
+          <Box
+            display={'grid'}
+            gridTemplateColumns={'repeat(auto-fit, minmax(250px, 1fr))'}
+            gap={'2rem'}
+            margin={'0 1rem'}
+          >
+            {characters.map((character) => {
+              return (
+                <CharacterCard
+                  key={character.id}
+                  id={character.id}
+                  name={character.name}
+                  image={character.image}
+                />
+              );
+            })}
+          </Box>
+          <Pagination
+            count={info?.pages}
+            page={page}
+            onChange={handleChange}
+          />
+        </Box>
+      )}
+    </>
   );
 };
--- src/src/features/Characters/hooks/useCharacterList.ts
+++ src/src/features/Characters/hooks/useCharacterList.ts
@@ -1,6 +1,6 @@
 import { ChangeEvent, useEffect, useState } from "react";
-import { getCharacters } from "../features/Characters/services/CharacterService";
-import { CharacterI, CharacterResponseI, InfoI } from "../interfaces/CharacterI";
+import { getCharacters } from "../services/CharacterService";
+import { CharacterI, CharacterResponseI, InfoI } from "../../../interfaces/CharacterI";
 
 interface ChangePaginationPropsI {
     page: number;
@@ -9,6 +9,7 @@ export const useCharacterList = () => {
     const [characters, setCharacters] = useState<CharacterI[]>([]);
     const [info, setInfo] = useState<InfoI | null>();
     const [page, setPage] = useState<number>(1);
+    const [loading, setLoading] = useState<boolean>(false)
 
     /**
     * @description Function that loads the API data
@@ -24,14 +25,16 @@ export const useCharacterList = () => {
    * @param value The number of page selected
    */
     const changePagination = ({ page }: ChangePaginationPropsI) => {
+        setLoading(true);
         getCharacters(page).then(({ results, info }: CharacterResponseI) => {
             setCharacters(results);
             setInfo(info);
             setPage(page);
+            setLoading(false);
         });
     };
 
-    return { characters, info, page, changePagination }
+    return { characters, info, page, changePagination, loading }
 
 
 }
