fafcc95a8edfaa0d5aa203f74061012553702686
Fernando <lgarcia@tredicom.com>
2023-02-22 12:19:30 -0600

    feat: :sparkles: Added SpinnerLoader

---
+++ src/src/components/LoadingSpinner/components/LoadingSpinner.tsx
@@ -0,0 +1,9 @@
+import "../styles/LoadingSpinner.css";
+
+export function LoadingSpinner() {
+  return (
+    <div className="spinner-container">
+      <div className="loading-spinner"></div>
+    </div>
+  );
+}
