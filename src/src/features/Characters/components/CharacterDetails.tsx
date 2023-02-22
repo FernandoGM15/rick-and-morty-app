2abdb2f3e89877d90f4d59bd33ebc1da99d3ec41
Fernando <lgarcia@tredicom.com>
2023-02-22 12:20:39 -0600

    feat: :sparkles: Implemented Character Details and LoadSpinner Integration

--- src/src/features/Characters/components/CharacterDetails.tsx
+++ src/src/features/Characters/components/CharacterDetails.tsx
@@ -1,16 +1,79 @@
-import { Typography } from "@mui/material";
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { CharacterI } from "../../../interfaces/CharacterI";
-import { getCharacter } from "../services/CharacterService";
+import { Box, List, ListItem, Typography } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { LoadingSpinner } from '../../../components/LoadingSpinner/components/LoadingSpinner';
+import { CharacterI } from '../../../interfaces/CharacterI';
+import { getCharacter } from '../services/CharacterService';
 
 export const CharacterDetails = () => {
-  const [character, setCharacter] = useState<CharacterI | null>();
+  const [character, setCharacter] = useState<CharacterI>();
+  const [loading, setLoading] = useState<boolean>(false);
   const { id } = useParams();
 
+  /**
+   * @description Get the character by Id obtained from params
+   */
   useEffect(() => {
     if (!id) return;
-    getCharacter(id).then((character) => setCharacter(character));
+    setLoading(true);
+    getCharacter(id).then((character) => {
+      setCharacter(character);
+      setLoading(false);
+    });
   }, []);
-  return <Typography variant="h1">{character?.name}</Typography>;
+  
+  return (
+    <>
+      {loading ? (
+        <LoadingSpinner />
+      ) : (
+        <Box
+          sx={{
+            height: '80vh',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            gap: '5rem',
+            margin: '0 3rem'
+          }}>
+          <Box
+            sx={{
+              width: '40%'
+            }}>
+            <img
+              width="100%"
+              src={character?.image}
+              alt={character?.name}
+            />
+          </Box>
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column'
+            }}>
+            <List>
+              <ListItem>
+                <Typography variant="h5">Name: {character?.name}</Typography>
+              </ListItem>
+              <ListItem>
+                <Typography variant="h5">Species: {character?.species}</Typography>
+              </ListItem>
+              <ListItem>
+                <Typography variant="h5">Gender: {character?.gender}</Typography>
+              </ListItem>
+              <ListItem>
+                <Typography variant="h5">Status: {character?.status}</Typography>
+              </ListItem>
+              <ListItem>
+                <Typography variant="h5">Location: {character?.location.name}</Typography>
+              </ListItem>
+              <ListItem>
+                <Typography variant="h5">Origin: {character?.origin.name}</Typography>
+              </ListItem>
+            </List>
+          </Box>
+        </Box>
+      )}
+    </>
+  );
 };
